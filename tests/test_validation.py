72497fb19ea839a01bbf6ebd6c08605e3306bbf0
Dion <dion.bejtullahu2005@gmail.com>
2026-08-06 16:18:02 +0200

    Update test_validation.py

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -3,14 +3,11 @@ from src.validate import validate_time_series
 from src.extract import extract_excel
 from src.transform import transform
 
-
-
 def test_time_series():
 
     df_j, _ = extract_excel()
 
     df = transform(df_j)
-
 
     result = validate_time_series(df)
 
