750430ce56185f5663f40c49896955fe50f27b56
Dion <dion.bejtullahu2005@gmail.com>
2026-08-13 10:33:42 +0200

    Update strategy_simulation.py

--- src/strategy_simulation.py
+++ src/strategy_simulation.py
@@ -16,30 +16,14 @@ def simulate_nomination_strategies(df):
 
         temp = df.copy()
 
-        # ======================================================
         # 1. SIMULATED NOMINATION
-        # ======================================================
-
         temp["simulated_nomination"] = (
             temp["plan_mwh"]
             *
             (1 + margin)
         )
 
-        # ======================================================
         # 2. SIMULATED IMBALANCE
-        #
-        # Formula reale nga Excel:
-        #
-        # Imbalance =
-        # Plan
-        # - Production
-        # - Consumption
-        # + Production Plan
-        # + Uncover Sell
-        # - Uncover Buy
-        # ======================================================
-
         temp["simulated_imbalance"] = (
             temp["simulated_nomination"]
             - temp["production_mwh"]
@@ -47,34 +31,20 @@ def simulate_nomination_strategies(df):
             + temp["plan_mwh3"]
         )
 
-        # ======================================================
         # 3. ABSOLUTE IMBALANCE
-        #
-        # Për MAE përdorim madhësinë absolute të jobalancit.
-        # ======================================================
-
         temp["simulation_error_MWh"] = (
             temp["simulated_imbalance"]
             .abs()
         )
 
-        # ======================================================
         # 4. FINANCIAL COST
-        #
-        # Për krahasimin e riskut/kostos:
-        # |imbalance| * |price|
-        # ======================================================
-
         temp["simulation_cost_euro"] = (
             temp["simulation_error_MWh"]
             *
             temp["price"].abs()
         )
 
-        # ======================================================
         # 5. RESULTS
-        # ======================================================
-
         results.append(
             {
                 "Strategy": name,
